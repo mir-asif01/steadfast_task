4dd8ce7345bd3b7f81039c3152790aa601f6da6c
mir-asif01 <asifbsb2002@gmail.com>
2024-10-03 22:31:08 +0600

    mobile-first responsive navbar added, previous design changed

--- task-1/js/script.js
+++ task-1/js/script.js
@@ -3,11 +3,11 @@ const menu_icon = document.getElementById("menu-icon")
 const mobile_drawer = document.getElementById("mobile-drawer")
 
 menu_icon.addEventListener("click", () => {
-  mobile_drawer.classList.remove("mobile-drawer-hidden")
-  mobile_drawer.classList.add("mobile-drawer-visible")
+  mobile_drawer.classList.remove("hide-drawer")
+  mobile_drawer.classList.add("show-drawer")
 })
 
 document.getElementById("drawer-close-btn").addEventListener("click", () => {
-  mobile_drawer.classList.remove("mobile-drawer-visible")
-  mobile_drawer.classList.add("mobile-drawer-hidden")
+  mobile_drawer.classList.remove("show-drawer")
+  mobile_drawer.classList.add("hide-drawer")
 })
