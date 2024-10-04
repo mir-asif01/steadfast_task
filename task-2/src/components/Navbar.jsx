3b43ef39394711cc1771af7c8cfd2e83b2c66cc1
mir-asif01 <asifbsb2002@gmail.com>
2024-10-04 07:44:12 +0600

    task-2 desktop and tab view navbar added

--- task-2/src/components/Navbar.jsx
+++ task-2/src/components/Navbar.jsx
@@ -4,32 +4,37 @@ export default function Navbar() {
   const [openDrawer, setOpenDrawer] = useState(false)
   return (
     <>
-      <nav className="flex justify-between items-center py-[20px] px-[35px]">
+      <nav className="flex justify-between items-center py-[20px] px-[35px] relative bg-[#f5efff] shadow-md">
         <div className="text-[30px]">Logo</div>
-        <div className="relative">
+        <div className="">
           <div>
             <MdMenu
-              className="text-[30px]"
+              className="text-[30px] md:hidden"
               onClick={() => setOpenDrawer(true)}
             />
           </div>
-          <ul
-            className={`fixed h-[100vh] py-[30px] text-left px-[50px] bg-violet-500 transition-[right] 0.9s ${
-              openDrawer ? "top-0 right-0 z-10" : "top-0 right-[-1000px]"
-            }`}
-          >
-            {" "}
-            <li className="mx-[5px] my-[10px]">
-              <MdOutlineClose
-                onClick={() => setOpenDrawer(false)}
-                className="text-[30px]"
-              />
-            </li>
-            <li className="mx-[5px] my-[10px]">Home</li>
-            <li className="mx-[5px] my-[10px]">Courses</li>
-            <li className="mx-[5px] my-[10px]">Login</li>
+          <ul className="hidden md:flex justify-between items-center gap-10 text-lg">
+            <li>Home</li>
+            <li>Courses</li>
+            <li>Login</li>
           </ul>
         </div>
+        <ul
+          className={`fixed h-[100vh] py-[30px] text-left px-[50px] bg-violet-500 transition-[right] 0.9s ${
+            openDrawer ? "top-0 right-0 z-10" : "top-0 right-[-1000px]"
+          }`}
+        >
+          {" "}
+          <li className="mx-[5px] my-[10px]">
+            <MdOutlineClose
+              onClick={() => setOpenDrawer(false)}
+              className="text-[30px]"
+            />
+          </li>
+          <li className="mx-[5px] my-[10px]">Home</li>
+          <li className="mx-[5px] my-[10px]">Courses</li>
+          <li className="mx-[5px] my-[10px]">Login</li>
+        </ul>
       </nav>
     </>
   )
--- task-2/tailwind.config.js
+++ task-2/tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "550px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "1050px",
+      // => @media (min-width: 768px) { ... }
+    },
+    plugins: [],
   },
-  plugins: [],
 }
